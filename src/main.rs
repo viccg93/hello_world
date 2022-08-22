92e6d7a1be0e4399897f2ca3bd343646019bea0d
victor <vic.cg93@gmail.com>
2022-08-22 04:45:14 -0500

    Hola mundo en Rust

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+//tratandose de nombres de archivo, se prefiere usar underscore a camel en Rust
+//para compilar se usa rustc [filename.rs]
+//para ejecutar ./filename(compilado)
+
+fn main(){ //funcion especial y reservada main, marca el inicio del programa
+    println!("Hello world Rust!"); //macro que imprime mensaje (motivo del !)
+}
